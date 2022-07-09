2bd20ab958602c662a935c0d61738ca2992deb5b
Alejandro Cruz <hcruz0716@gmail.com>
2022-07-09 04:46:09 -0600

    Add images and carousel

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,17 @@
     <Home />
     <Frase />
     <Lugar />
+    <Fotos />
   </div>
 </template>
 <script>
 import Home from "../components/Home";
 import Lugar from "../components/Lugar";
 import Frase from "../components/Frase";
+import Fotos from "../components/Fotos.vue";
 
 export default {
   name: "IndexPage",
-  components: { Home, Lugar, Frase },
+  components: { Home, Lugar, Frase, Fotos },
 };
 </script>
