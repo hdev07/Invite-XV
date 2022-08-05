f5a7c8383c991fdd9df80ae66995dc7fdb66af5b
Alejandro Cruz <hcruz0716@gmail.com>
2022-08-05 00:14:12 -0500

    Finish!?

--- components/Final.vue
+++ components/Final.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="h-screen w-screen bg-img-final flex justify-center">
-    <div class="z-10 mt-96">
-      <div class="mt-56">
-        <p class="cursive text-4xl text-center text-white m-4 mb-12 font-bold uppercase">
+    <div class="z-10 mt-80">
+      <div class="mt-72">
+        <p class="cursive text-4xl text-center text-white m-4 mb-4 font-bold uppercase">
           Sofía
         </p>
-        <p class="cursive text-4xl text-center text-white m-4 mb-12 font-bold uppercase">
+        <p class="cursive text-2xl text-center text-white m-2 font-bold">
           Celebremos juntos este día especial!
         </p>
+        <div class="backdrop-blur-lg bg-white/30">
+          <img src="../assets/img/Logo.png" alt="Logo_Sofia" class="mx-auto mt-8 w-1/2" />
+        </div>
       </div>
     </div>
   </div>
